5a094970e0cd61cf3d2414958ce90d2774590f2d
unknown <russelhkamal@gmail.com>
2022-05-22 19:58:56 -0400

    added routes to index and notes

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ const app = express();
 const port = 3001;
 // gives a path to folder containing our html
 const htmlPath = path.join(__dirname, "/public");
-const notes = require("./db/db.json");
+// accesses all notes saved as JSON objects
+const savedNotes = require("./db/db.json");
 const exp = require("constants");
+const req = require("express/lib/request");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+} );
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
